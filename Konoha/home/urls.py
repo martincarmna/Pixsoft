03a6530fe05e3cacd8fa4378bfb7db6e6f793ae7
martincarmna <mcarmonapalestina@gmail.com>
2025-12-04 20:12:38 -0600

    Interfaz funcionando login

--- Konoha/home/urls.py
+++ Konoha/home/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index.html'),
-    path('login/', views.login, name='login.html'),
-    path('home/', views.home, name='home.html'),
-    path('loginuser/', views.loginuser, name='loginuser.html'),
+    path('', views.home, name='index'),
+    path('home/', views.home, name='home'),
+    path('loginuser/', views.loginuser, name='loginuser'),
 ]
