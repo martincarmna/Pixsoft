b12f4d92f0f8e738a0f98dbf0c6c387bf5912bc4
1JORSH <cocajorge453@gmail.com>
2025-12-04 21:25:21 -0600

    jorge

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='index.html'),
+    path('login/', views.login, name='login.html'),
+    path('home/', views.home, name='home.html'),
+    path('loginuser/', views.loginuser, name='loginuser.html'),
+]
---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def login(request):
+    return render (request, 'login.html')
+
+def home(request):
+    return render (request, 'index.html')
+def loginuser(request):
+    return render (request, 'loginuser.html')
